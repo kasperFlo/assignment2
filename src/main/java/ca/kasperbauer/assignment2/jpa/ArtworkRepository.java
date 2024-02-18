d2d9d096e4ed894d52df2ef25447dac2bf5fff23
kasperFlo <20kasperbauefr2@gmail.com>
2024-02-17 22:49:55 -0500

    working on running

--- src/main/java/ca/kasperbauer/assignment2/jpa/ArtworkRepository.java
+++
@@ -1,6 +0,0 @@
-package ca.kasperbauer.assignment2.jpa;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ArtworkRepository extends JpaRepository<Artwork, Integer> {
-}
